3e978c1893daa3044672ba6cc5ef37b80e758427
Vladislav <novlad26@gmail.com>
2019-03-22 18:55:13 +0300

    Added new clone_project to prepare.sh; Updated .gitignore

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -32,8 +32,9 @@ clone_project()
 stage "Clone projects"
 
 clone_project https://github.com/ShunkevichDV/sc-machine.git sc-machine scp_stable
-clone_project https://github.com/Ivan-Zhukau/sc-web.git sc-web master
-clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb master
+clone_project https://github.com/kononir/IHS-by-car-sc-web.git sc-web master
+clone_project https://github.com/kononir/IHS-by-car-ims.ostis.kb.git ims.ostis.kb master
+clone_project https://github.com/kononir/IHS-by-car-ims.ostis.kb kb master
 
 stage "Prepare projects"
 
